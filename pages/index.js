13a8c299ffd54d9f9dae4cdcd91661570b03e846
sandarma <sandarminaye@live.com>
2021-05-12 09:56:38 +0800

    just commit

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,9 @@ const HomeOne = () => {
             <p>
             With cordial support from Burmese Community Development Collaboration (BCDC), the funds will be collected through the existing BCDC platform, and you will receive the payment confirmation email from BCDC.
             <br/>
+            <span style={{fontSize: 14}}>
             လှူဒါန်းငွေများကို ဩစတြေးလျနိုင်ငံ မြန်မာဖွံ့ဖြိုးရေးပူးပေါင်းဆောင်ရွက်မှု အသင်း (Burmese Community Development Collaboration - BCDC) မှ တဆင့် ကောက်ခံမှာ ဖြစ်ပြီး BCDC မှ လက်ခံ ရရှိကြောင်း ဖြတ်ပိုင်းကို email ဖြင့် ပေးပို့မှာ ဖြစ်ပါတယ်။
+            </span>
             </p>
             <p>
               <Link href="https://www.userroll.com/site/register/t201jaf"> 
